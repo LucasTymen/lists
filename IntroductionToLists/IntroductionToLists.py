da44917f2fe6046e66772438b438e4003c1e97ae
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-01 16:44:59 +0200

    1_list_intro

---
+++ IntroductionToLists/IntroductionToLists.py
@@ -0,0 +1,58 @@
+"""
+Introduction to Lists
+What is a List?
+
+In programming, it is common to want to work with collections of data. In Python, a list is one of the many built-in data structures that allows us to work with a collection of data in sequential order.
+
+Suppose we want to make a list of the heights of students in a class:
+
+    Noelle is 61 inches tall
+    Ava is 70 inches tall
+    Sam is 67 inches tall
+    Mia is 64 inches tall
+
+In Python, we can create a variable called heights to store these integers into a list:
+"""
+heights = [61, 70, 67, 64]
+"""
+Notice that:
+
+    A list begins and ends with square brackets ([ and ]).
+    Each item (i.e., 67 or 70) is separated by a comma (,)
+    It’s considered good practice to insert a space () after each comma, but your code will run just fine if you forget the space.
+
+Let’s write our own list!
+Instructions
+1.
+
+Examine the existing list heights in your code editor.
+
+A new student just joined the class!
+
+    Chloe is 65 inches tall
+
+Add Chloe’s height to the end of the list heights.
+
+When adding a new element to a list, don’t forget the comma to separate the values!
+2.
+
+Remove the # in front of the definition of the list broken_heights. If you run this code, you’ll get an error in your terminal:
+
+SyntaxError: invalid syntax
+
+Add commas (,) to broken_heights so that it runs without errors.
+
+Did you add commas ( , ) between each element?
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+Community Forums
+
+Here are some helpful links to the top questions asked by coders about this exercise:
+
+    For this exercise, how can I edit the existing list variable in the code to add new data items?
+
+Why do lists use brackets instead of parenthesis?
+How can I look up the index of an item in my list?
+
+Still have questions? View this exercise's thread in the Codecademy Forums.
+"""
---
+++ IntroductionToLists/files/_test.py
@@ -0,0 +1,3 @@
+load_file_in_context('script.py')
+full_equality('heights', [61, 70, 67, 64, 65])
+pass_tests()
