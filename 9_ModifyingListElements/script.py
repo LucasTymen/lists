894f3a9a5a52572223feb2ba43c2c17f72729c1b
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-03 16:37:00 +0200

    9_ModifyingListElements

---
+++ 9_ModifyingListElements/script.py
@@ -0,0 +1,14 @@
+# Your code below:
+garden = ["Tomatoes", "Green Beans", "Cauliflower", "Grapes"]
+garden[2] = "Strawberries"
+
+print(garden)
+
+garden_waitlist =["Jiho", "Adam", "Sonny", "Alisha"]
+print(garden_waitlist)
+
+garden_waitlist[1] = "Calla"
+print(garden_waitlist)
+
+garden_waitlist[-1] = "alex"
+print(garden_waitlist)
