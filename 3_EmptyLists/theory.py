30101fbc0ce94f6a8babb6a87585392daa0258f3
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-03 10:45:29 +0200

    3_empty_list

---
+++ 3_EmptyLists/theory.py
@@ -0,0 +1,29 @@
+"""
+Introduction to Lists
+Empty Lists
+
+A list doesn’t have to contain anything. You can create an empty list like this:
+
+empty_list = []
+
+Why would we create an empty list?
+
+Usually, it’s because we’re planning on filling it up later based on some other input. We’ll talk about two ways of filling up a list in the next exercise.
+
+Let’s practice writing an empty list!
+Instructions
+1.
+
+Create an empty list and call it my_empty_list. Don’t put anything in the list just yet.
+
+Remember that a list begins and ends with square brackets ([ and ]) and does not have to contain any elements.
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+Community Forums
+
+Here are some helpful links to the top questions asked by coders about this exercise:
+
+    For this exercise, are there any other options for creating an empty list?
+
+Still have questions? View this exercise's thread in the Codecademy Forums.
+"""
